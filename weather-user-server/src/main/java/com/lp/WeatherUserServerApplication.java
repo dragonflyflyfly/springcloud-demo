79e890ec6d6aa9991d342df887b4114263f9dc87
dragonflyflyfly <225733616@qq.com>
2018-10-22 20:16:25 +0800

    更新了内容

--- weather-user-server/src/main/java/com/lp/WeatherUserServerApplication.java
+++ weather-user-server/src/main/java/com/lp/WeatherUserServerApplication.java
@@ -2,9 +2,8 @@ package com.lp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class WeatherUserServerApplication {
 
--- weather-user-server/src/main/java/com/lp/controller/UserController.java
+++ weather-user-server/src/main/java/com/lp/controller/UserController.java
@@ -3,10 +3,12 @@ package com.lp.controller;
 import com.lp.domain.User;
 import com.lp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class UserController {
 
@@ -17,5 +19,16 @@ public class UserController {
     User getUserById(@PathVariable("id") Integer id) {
         return userService.getUserById(id);
     }
+
+
+
+
+/*    @Value("${demo}")
+    private String demo;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return this.demo;
+    }*/
 
 }
